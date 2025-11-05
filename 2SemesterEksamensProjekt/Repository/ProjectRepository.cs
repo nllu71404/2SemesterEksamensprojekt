4b1f51e990745ff0bb2a92747b824c0299c0ac1d
Melinda Allen <msal71396@edu.ucl.dk>
2025-11-05 20:46:51 +0100

    Company

--- 2SemesterEksamensProjekt/Repository/ProjectRepository.cs
+++ 2SemesterEksamensProjekt/Repository/ProjectRepository.cs
@@ -9,31 +9,6 @@ namespace _2SemesterEksamensProjekt.Repository
 {
     public class ProjectRepository : BaseRepository
     {
-        /*
-        public List<Project> GetAllProjects()
-        {
-            /*
-            return ExecuteSafe(conn =>
-            {
-                var projects = new List<Project>();
-                using var cmd = new Microsoft.Data.SqlClient.SqlCommand(
-                    "SELECT ProjectId, ProjectTitle, ProjectStatys, Company FROM Project", conn);
-                using var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    var project = new Project
-                    {
-                        ProjectId = reader.GetInt32(0),
-                        ProjectTitle = reader.IsDBNull(1) ? "" : reader.GetString(1),
-                        ProjectStatus = reader.GetString(2),
-                        Company = reader.GetString(3),
-                    };
-                    projects.Add(project);
-                }
-                return projects;
-            }) ?? new List<Project>();
-            
-        }
-        */
+        
     }
 }
--- 2SemesterEksamensProjekt/Repository/TopicRepository.cs
+++ 2SemesterEksamensProjekt/Repository/TopicRepository.cs
@@ -9,24 +9,6 @@ namespace _2SemesterEksamensProjekt.Repository
 {
     public class TopicRepository : BaseRepository
     {
-        public List<Topic> GetAllTopics()
-        {
-            return ExecuteSafe(conn =>
-            {
-                var topics = new List<Topic>();
-                using var cmd = new Microsoft.Data.SqlClient.SqlCommand(
-                    "SELECT TopicId, TopicDescription FROM Topic", conn);
-                using var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    var topic = new Topic
-                    {
-                        TopicId = reader.GetInt32(0),
-                        TopicDescription = reader.IsDBNull(1) ? "" : reader.GetString(1)
-                    };
-                }
-                return topics;
-            }) ?? new List<Topic>();
-        }
+       
     }
 }
