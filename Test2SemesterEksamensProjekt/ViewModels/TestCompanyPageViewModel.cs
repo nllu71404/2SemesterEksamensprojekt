1d5a5e5cb916864e33405e8110b8c261cefde28d
Lauraerunik <168111337+Lauraerunik@users.noreply.github.com>
2025-11-27 17:27:10 +0100

    Første test af CompanyPageViewModel virker

---
+++ Test2SemesterEksamensProjekt/ViewModels/TestCompanyPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿
+using _2SemesterEksamensProjekt.Repository;
+using _2SemesterEksamensProjekt.ViewModels;
+using Moq;
+
+namespace Test2SemesterEksamensProjekt.ViewModels
+{
+    [TestClass]
+    public sealed class TestCompanyPageViewModel
+    {
+        private Mock<ICompanyRepository> companyRepositoryMock;
+
+        [TestInitialize]
+        public void Init()
+        { 
+            companyRepositoryMock = new Mock<ICompanyRepository>();
+        }
+
+        [TestMethod]
+        public void Constructor_LoadsCompaniesToObservableCollection()
+        {
+            // Arrange
+            companyRepositoryMock
+                .Setup(x => x.GetAllCompanies())
+                .Returns(new List<_2SemesterEksamensProjekt.Models.Company>
+                {
+                                new _2SemesterEksamensProjekt.Models.Company { CompanyId = 1, CompanyName = "Test Company 1" },
+                                new _2SemesterEksamensProjekt.Models.Company { CompanyId = 2, CompanyName = "Test Company 2" }
+                });
+            
+            // Act
+            var testingViewModel = new CompanyPageViewModel(companyRepositoryMock.Object);
+
+            // Assert
+            Assert.AreEqual(2, testingViewModel.Companies.Count);
+            Assert.AreEqual("Test Company 1", testingViewModel.Companies[0].CompanyName);
+        }
+    }
+}
