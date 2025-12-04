e30e37bb3544e963e017e8f6401f76c80a403778
Melinda Allen <msal71396@edu.ucl.dk>
2025-12-04 14:30:01 +0100

    Parameter

--- 2SemesterEksamensProjekt/Repository/ITimeRecordRepository.cs
+++ 2SemesterEksamensProjekt/Repository/ITimeRecordRepository.cs
@@ -10,7 +10,7 @@ namespace _2SemesterEksamensProjekt.Repository
     public interface ITimeRecordRepository
     {
         List<TimeRecord> GetAllTimeRecords();
-        List<TimeRecord> GetTimeRecordByFilter(int timerId);
+        List<TimeRecord> GetTimeRecordByFilter(int? companyId, int? projectId, int? topicId, int? month, int? year);
         int SaveNewTimeRecord(TimeRecord timer);
     }
 }
