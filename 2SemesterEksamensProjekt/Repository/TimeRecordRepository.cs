e2484e004850d14373de85a9304412ea721d0f07
Lauraerunik <168111337+Lauraerunik@users.noreply.github.com>
2025-12-04 14:29:11 +0100

    Ting

--- 2SemesterEksamensProjekt/Repository/TimeRecordRepository.cs
+++ 2SemesterEksamensProjekt/Repository/TimeRecordRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using _2SemesterEksamensProjekt.Models;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _2SemesterEksamensProjekt.Models;
-using Microsoft.Data.SqlClient;
 
 namespace _2SemesterEksamensProjekt.Repository
 {
@@ -37,32 +38,42 @@ namespace _2SemesterEksamensProjekt.Repository
                 return timers;
             });
         }
-        public List<TimeRecord> GetTimeRecordByFilter(string month)
+        public List<TimeRecord> GetTimeRecordByFilter(int? companyId, int? projectId, int? topicId, int? month, int? year)
         {
-            return ExecuteSafe(conn =>
+            List<TimeRecord> result = new List<TimeRecord>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = new SqlCommand("uspFilterTimeRecords", conn))
             {
-                var timers = new List<TimeRecord>();
-                using var cmd = new SqlCommand("SKRIV STORED PROCEDURES", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("SKRIV STORED PROCEDURES", );
 
-                using var reader = cmd.ExecuteReader();
-                while (reader.Read())
+                cmd.Parameters.AddWithValue("@CompanyId", (object?)companyId ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@ProjectId", (object?)projectId ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@TopicId", (object?)topicId ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@Month", (object?)month ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@Year", (object?)year ?? DBNull.Value);
+
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
                 {
-                    timers.Add(new TimeRecord
+                    while (reader.Read())
                     {
-                        TimerId = reader.GetInt32(0),
-                        TimerName = reader.GetString(1),
-                        ElapsedTime = TimeSpan.FromSeconds(reader.GetInt32(2)),
-                        StartTime = reader.GetDateTime(3),
-                        CompanyId = reader.GetInt32(4),
-                        ProjectId = reader.GetInt32(5),
-                        TopicId = reader.GetInt32(6),
-                    });
-                }
+                        TimeRecord tr = new TimeRecord();
+                        tr.TimerId = reader.GetInt32(0);
+                        tr.TimerName = reader.GetString(1);
+                        tr.ElapsedTime = reader.GetTimeSpan(2);
+                        tr.StartTime = reader.GetDateTime(3);
+                        tr.CompanyId = reader.IsDBNull(4) ? null : reader.GetInt32(4);
+                        tr.ProjectId = reader.IsDBNull(5) ? null : reader.GetInt32(5);
+                        tr.TopicId = reader.IsDBNull(6) ? null : reader.GetInt32(6);
+                        tr.Note = reader.IsDBNull(7) ? null : reader.GetString(7);
 
-                return timers;
-            });
+                        result.Add(tr);
+                    }
+                }
+            }
+
+            return result;
         }
         public int SaveNewTimeRecord(TimeRecord timer)
         {
