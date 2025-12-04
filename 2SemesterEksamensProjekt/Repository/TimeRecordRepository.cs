fb64d6c49d3baf79d442f579caa298240b1814f7
Melinda Allen <msal71396@edu.ucl.dk>
2025-12-04 14:56:11 +0100

    OverViewArbejde

--- 2SemesterEksamensProjekt/Repository/TimeRecordRepository.cs
+++ 2SemesterEksamensProjekt/Repository/TimeRecordRepository.cs
@@ -16,7 +16,7 @@ namespace _2SemesterEksamensProjekt.Repository
         {
             return ExecuteSafe(conn =>
             {
-                var timers = new List<TimeRecord>();
+                var timeRecords = new List<TimeRecord>();
 
                 using var cmd = new SqlCommand(
                         "SELECT * FROM dbo.vwSelectAllTimeRecord", conn);
@@ -24,7 +24,7 @@ namespace _2SemesterEksamensProjekt.Repository
                 using var reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    timers.Add(new TimeRecord
+                    timeRecords.Add(new TimeRecord
                     {
                         TimerId = reader.GetInt32(0),
                         TimerName = reader.GetString(1),
@@ -35,7 +35,7 @@ namespace _2SemesterEksamensProjekt.Repository
                         TopicId = reader.GetInt32(6),
                     });
                 }
-                return timers;
+                return timeRecords;
             });
         }
         public List<TimeRecord> GetTimeRecordByFilter(int? companyId, int? projectId, int? topicId, int? month, int? year)
@@ -58,24 +58,24 @@ namespace _2SemesterEksamensProjekt.Repository
                 {
                     while (reader.Read())
                     {
-                        TimeRecord tr = new TimeRecord();
-                        tr.TimerId = reader.GetInt32(0);
-                        tr.TimerName = reader.GetString(1);
-                        tr.ElapsedTime = reader.GetTimeSpan(2);
-                        tr.StartTime = reader.GetDateTime(3);
-                        tr.CompanyId = reader.IsDBNull(4) ? null : reader.GetInt32(4);
-                        tr.ProjectId = reader.IsDBNull(5) ? null : reader.GetInt32(5);
-                        tr.TopicId = reader.IsDBNull(6) ? null : reader.GetInt32(6);
-                        tr.Note = reader.IsDBNull(7) ? null : reader.GetString(7);
+                        TimeRecord timeRecord = new TimeRecord();
+                        timeRecord.TimerId = reader.GetInt32(0);
+                        timeRecord.TimerName = reader.GetString(1);
+                        timeRecord.ElapsedTime = reader.GetTimeSpan(2);
+                        timeRecord.StartTime = reader.GetDateTime(3);
+                        timeRecord.CompanyId = reader.IsDBNull(4) ? null : reader.GetInt32(4);
+                        timeRecord.ProjectId = reader.IsDBNull(5) ? null : reader.GetInt32(5);
+                        timeRecord.TopicId = reader.IsDBNull(6) ? null : reader.GetInt32(6);
+                        timeRecord.Note = reader.IsDBNull(7) ? null : reader.GetString(7);
 
-                        result.Add(tr);
+                        result.Add(timeRecord);
                     }
                 }
             }
 
             return result;
         }
-        public int SaveNewTimeRecord(TimeRecord timer)
+        public int SaveNewTimeRecord(TimeRecord timeRecord)
         {
             return ExecuteSafe(conn =>
             {
@@ -83,24 +83,24 @@ namespace _2SemesterEksamensProjekt.Repository
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 cmd.Parameters.Add("@TimerName", SqlDbType.NVarChar, 100).Value =
-                string.IsNullOrEmpty(timer.TimerName) ? throw new ArgumentException("TimerName må ikke være tom") : timer.TimerName;
+                string.IsNullOrEmpty(timeRecord.TimerName) ? throw new ArgumentException("TimerName må ikke være tom") : timeRecord.TimerName;
 
-                cmd.Parameters.Add("@ElapsedTime", SqlDbType.Time).Value = timer.ElapsedTime;
+                cmd.Parameters.Add("@ElapsedTime", SqlDbType.Time).Value = timeRecord.ElapsedTime;
 
                 cmd.Parameters.Add("@StartTime", SqlDbType.DateTime).Value =
-                    timer.StartTime != default ? timer.StartTime : DateTime.Now;
+                    timeRecord.StartTime != default ? timeRecord.StartTime : DateTime.Now;
 
                 cmd.Parameters.Add("@CompanyId", SqlDbType.Int).Value =
-                    timer.CompanyId.HasValue ? timer.CompanyId.Value : DBNull.Value;
+                    timeRecord.CompanyId.HasValue ? timeRecord.CompanyId.Value : DBNull.Value;
 
                 cmd.Parameters.Add("@ProjectId", SqlDbType.Int).Value =
-                    timer.ProjectId.HasValue ? timer.ProjectId.Value : DBNull.Value;
+                        timeRecord.ProjectId.HasValue ? timeRecord.ProjectId.Value : DBNull.Value;
 
                 cmd.Parameters.Add("@TopicId", SqlDbType.Int).Value =
-                    timer.TopicId.HasValue ? timer.TopicId.Value : DBNull.Value;
+                        timeRecord.TopicId.HasValue ? timeRecord.TopicId.Value : DBNull.Value;
 
                 cmd.Parameters.Add("@Note", SqlDbType.NVarChar).Value =
-                     string.IsNullOrEmpty(timer.Note) ? DBNull.Value : timer.Note;
+                     string.IsNullOrEmpty(timeRecord.Note) ? DBNull.Value : timeRecord.Note;
                 object result = cmd.ExecuteScalar();
                 return Convert.ToInt32(result);
 
--- 2SemesterEksamensProjekt/ViewModels/OverViewPageViewModel.cs
+++ 2SemesterEksamensProjekt/ViewModels/OverViewPageViewModel.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _2SemesterEksamensProjekt.Commands;
 using _2SemesterEksamensProjekt.Models;
 using _2SemesterEksamensProjekt.Repository;
 
@@ -13,12 +14,18 @@ namespace _2SemesterEksamensProjekt.ViewModels
     public class OverViewPageViewModel : BaseViewModel
     {
         //Fields
-        private readonly TimeRecordRepository _timeRecordRepo;
+        private readonly ITimeRecordRepository _timeRecordRepo;
+        private readonly ICompanyRepository _companyRepo;
+        private readonly IProjectRepository _projectRepo;
+        private readonly ITopicRepository _topicRepo;
 
         //Observablecollection
-        public ObservableCollection<TimeRecord> GetAllTimers {get; set;}
-        public ObservableCollection<TimeRecord> GetTimeRecordByMonth { get; set; }
+        public ObservableCollection<TimeRecord> TimeRecords {get; set;}
+        public ObservableCollection<Company> Companies { get; set; }
+        public ObservableCollection<Project> Projects { get; set; }
+        public ObservableCollection<Topic> Topics { get; set; }
         public ObservableCollection<string> Month { get; set; }
+        public ObservableCollection<int> Year { get; set; }
 
         //Properties til søgekriterier
         private string? _selectedMonth;
@@ -26,6 +33,65 @@ namespace _2SemesterEksamensProjekt.ViewModels
         {
             get => _selectedMonth;
             set => SetProperty(ref _selectedMonth, value);
+        }
+        private int? _selectedYear;
+        public int? SelectedYear
+        {
+            get => _selectedYear;
+            set => SetProperty(ref _selectedYear, value);
+        }
+        private string? _selectedCompany;
+        public string? SelectedCompany
+        {
+            get => _selectedCompany;
+            set => SetProperty(ref _selectedCompany, value);
+        }
+        private string? _selectedProject;
+        public string? SelectedProject
+        {
+            get => _selectedProject;
+            set => SetProperty(ref _selectedProject, value);
+        }
+        private string? _selectedTopic;
+        public string? SelectedTopic
+        {
+            get => _selectedProject;
+            set => SetProperty(ref _selectedProject, value);
+        }
+
+
+        //Commands
+        public RelayCommand ApplyFilterCommand { get; }
+
+        //Constructor
+        public OverViewPageViewModel(TimeRecord timeRecord, ITimeRecordRepository timeRecordRepository, 
+            ICompanyRepository companyRepository, IProjectRepository projectRepository, ITopicRepository topicRepository)
+        {
+            this._timeRecordRepo = timeRecordRepository;
+            foreach (var timeRecords in timeRecordRepository.GetAllTimeRecords())
+            {
+                TimeRecords.Add(timeRecord);
+            }
+
+            _timeRecordRepo = timeRecordRepository;
+            _companyRepo = companyRepository;
+            _projectRepo = projectRepository;
+            _topicRepo = topicRepository;
+
+
+            TimeRecords = new ObservableCollection<TimeRecord>();
+            Companies = new ObservableCollection<Company>();
+            Projects = new ObservableCollection<Project>();
+            Topics = new ObservableCollection<Topic>();
+
+            ApplyFilterCommand = new RelayCommand(_ => ApplyFilter());
+        }
+
+        //Methode
+
+        private void ApplyFilter()
+        {
+
         }
 
 
